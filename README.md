bb148376013f05de6f2108bfadf6ec75dd9581b2
mojianpo <mojianpo@gmail.com>
2020-11-27 09:00:04 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# chrome-besblocker
+## 使用说明
+
+- Change the response of the request.
+- 用来更改页面请求响应的内容。通过指定规则，您可以把请求映射到其他的URL。
+
+**现在就开始使用BES Blocker**
+
+### 安装说明
+
+- (chrome://extensions/)通过管理扩展程序，打开开发者模式，选择加载已解压的扩展程序（选择当前的目录即可）
+
+- PS：源码在手，您可以随意的调整完善。
+- 如有问题或者需求可联系：jianpo.mo@dev.bessystem.com
+ 
---
+++ background.js
@@ -0,0 +1,96 @@
+console.log('start BES Blocker');
+var _bes_typeMap = {
+    "txt"   : "text/plain",
+    "html"  : "text/html",
+    "css"   : "text/css",
+    "js"    : "text/javascript",
+    "json"  : "text/json",
+    "xml"   : "text/xml",
+    "jpg"   : "image/jpeg",
+    "gif"   : "image/gif",
+    "png"   : "image/png",
+    "webp"  : "image/webp"
+}
+
+function getBesBlockMap() {
+    let data = localStorage.getItem("BesBlockerMap") || '';
+    if(data){
+        return JSON.parse(data) || []
+    } else {
+        return []
+    } 
+}
+
+function setBesBlockMap(arr) {
+    localStorage.setItem("BesBlockerMap", JSON.stringify(arr));
+}
+
+function getLocalFileUrl(url) {
+    var arr = url.split('.');
+    var type = arr[arr.length-1];
+    var xhr = new XMLHttpRequest();
+    xhr.open('get', url, false);
+    xhr.send(null);
+    var content = xhr.responseText || xhr.responseXML;
+    if (!content) {
+        return false;
+    }
+    content = encodeURIComponent(
+        type === 'js' ?content.replace(/[\u0080-\uffff]/g, function($0) {
+            var str = $0.charCodeAt(0).toString(16);
+            return "\\u" + '00000'.substr(0, 4 - str.length) + str;
+        }) : content
+    );
+    return ("data:" + (_bes_typeMap[type] || _bes_typeMap.txt) + ";charset=utf-8," + content);
+}
+
+chrome.webRequest.onBeforeRequest.addListener(function (request) {
+        var url = request.url;
+        var items = getBesBlockMap()
+        for (var i = 0, len = items.length; i < len; i++) {
+            var item = items[i];
+            if(item.checked){
+                if(url === item.req && !/^file:\/\//.test(item.res)){
+                    url = item.res
+                }else {
+                    
+                    var reg = new RegExp(item.req, 'gi');
+                    if (typeof item.res === 'string' && reg.test(url)) {
+                        if (!/^file:\/\//.test(item.res)) {
+                            do {
+                                url = url.replace(reg, item.res);
+                            } while (reg.test(url))
+                        } else {
+                            do {
+                                url = getLocalFileUrl(url.replace(reg, item.res));
+                            } while (reg.test(url))
+                        }
+                    }
+                   
+               }
+           }
+        }
+        if((url === '403' || url === '400' || url === '500')){
+            return {redirectUrl: chrome.extension.getURL("403.html")};
+        }
+        return url === request.url ? {} : { redirectUrl: url };
+    }, {
+        urls: [
+            "<all_urls>"
+        ], 
+        types: [
+            "main_frame", 
+            "sub_frame", 
+            "stylesheet", 
+            "script", 
+            "image", 
+            "object", 
+            "xmlhttprequest", 
+            "other"
+        ]
+    },
+    ["blocking"]
+);
+
+console.log('end BES Blocker');
+
